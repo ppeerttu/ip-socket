87046bf82f7bc37345ec48898cf2066199d172a5
Perttu Kärnä <Perttu@Perttu-MacBook-Air.local>
2017-04-19 20:16:17 +0300

    Basic functionality almost done

--- sandbox.py
+++ sandbox.py
@@ -6,7 +6,7 @@ import struct
 class UdpSocket:
     'Used for sending UDP datagrams'
 
-    # Constructor - this function initializes the socket object
+    # Constructor - this method initializes the socket object
     def __init__(self, cid, sock=None):
         if sock is None:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,12 +14,12 @@ class UdpSocket:
             self.sock = sock
         self.cid = cid
 
-    # This function is propably no needed since the udp socket can receive datagrams without binding
+    # This method is propably no needed since the udp socket can receive datagrams without binding
     def bind(self, host, port):
         self.sock.bind((host, port))
 
-    # This function sends the message it gets as a parameter to ii.virtues.fi:23432
-    # However, static receiver must be replaced with dynamic receiver i.e. in __init__ function
+    # This mehtod sends the message it gets as a parameter to ii.virtues.fi:23432
+    # However, static receiver must be replaced with dynamic receiver i.e. in __init__ method
     def send(self, message):
         
         # Calculate the length of the actual message in bytes
@@ -44,8 +44,9 @@ class UdpSocket:
 
         # Unpack the raw data into understantable form
         try:
-            print data
-            cid, ack, eom, remaining, length, message = struct.unpack('!8s??HH64s', data)
+            cid, ack, eom, remaining, length, message = struct.unpack('!8s??HH64s', data[0])
+            message = message[:length]
+            return (message, eom)
         except Exception as e:
             print e
         
@@ -53,27 +54,27 @@ class UdpSocket:
 class TcpSocket:
     'Used for the first transmission via TCP'
 
-    # Constructor - this function initializes the socket object
+    # Constructor - this method initializes the socket object
     def __init__(self, sock=None):
         if sock is None:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
 
-    # This function sets up the connection - you need to call this in order to send or receive anything
+    # This method sets up the connection - you need to call this in order to send or receive anything
     def connect(self, host, port):
-        self.sock.connect((host,port))
+        self.sock.connect((host, port))
 
-    # This function sends blindly (no validation) message to the receiver
+    # This method sends blindly (no validation) message to the receiver
     def send(self, msg):
         self.sock.send(msg)
 
-    # This function receives blindly (max 2048 bytes) message
+    # This method receives blindly (max 2048 bytes) message
     def receive(self):
         chunk = self.sock.recv(2048)
         return chunk            
 
-    # This function closes the TCP connection
+    # This method closes the TCP connection
     # However, we should investigate whether this is enough for proper closing of TCP connection
     def close(self):
         self.sock.close()
@@ -81,34 +82,65 @@ class TcpSocket:
 
 
 ############################### APPLICATION LOGIC
-############################### Add functions used in main right below this
+############################### Add methods used in main right below this
 ############################### so that they are introduced before actually used
 
+# This method takes string as parameter, splits it into list of words by whitespaces
+# and passes the list to parse_to_string(list) -method, which ties the list of words
+# into one string and returns it
+def reverse_message(message):
+    size = len(message.split())
+    duplicate = message.split()
+    duplicate.reverse()
+    # print duplicate
+    return parse_to_string(duplicate)
+
+def parse_to_string(list):
+    newstr = ""
+    for n in range(len(list)):
+        newstr += list[n]
+        if n < (len(list) - 1):
+            newstr += " "
+    return newstr
+
+# For developement purposes address and port hardcoded
 address = "ii.virtues.fi"
 tcpPort = 10000
 
+# Creating an instance of TCP Socket, connecting it to the server and sending a message
 tsocket = TcpSocket()
-
-
-
-
 tsocket.connect(address, tcpPort)
 tsocket.send("HELLO\r\n")
+
+# Catching and printing the response from TCP Socket
 received = tsocket.receive()
 print "Received: ", received
+
+# Taking the necessary parameters out of the message (cid and UDP port)
 msg, cid, udpPort = received.split()
 print msg
 print cid
 print udpPort
 
+# Now we can create an instance of UDP Socket
+# Note that at this point we should input the UDP port to the socket - at the moment it is hardcoded
 usocket = UdpSocket(cid)
 
 try:
     hellomsg = "Hello from "
     hellomsg += cid
-    print "Hellomessage: ", hellomsg
     usocket.send(hellomsg)
-    usocket.receive()
+    while True:
+        message, eof = usocket.receive()
+        print "Eof: ", eof
+        if eof:
+            print "Last message: ", message
+            break
+        else:
+            revmsg = reverse_message(message)
+            print "New message to be sent: ", revmsg
+            usocket.send(revmsg)
+
 except Exception as e:
     print "Something is wrong with your class: ", e
 
